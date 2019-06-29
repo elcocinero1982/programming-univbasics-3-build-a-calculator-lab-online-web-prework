83e2ba85383a9575b8096323ab2b8bc5575a616c
jose luis <cortezluisjose@gmail.com>
2019-06-29 02:22:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,18 @@ num1 - num2
 end
 
 def division(num1, num2)
-
+puts num1, num2
+num1 / num2
 end
 
 def multiplication(num1, num2)
-
+puts num1, num2
+num1 * num2
 end
 
 def modulo(num1, num2)
-
+puts num1, num2
+num1 
 end
 
 def square_root(num)
